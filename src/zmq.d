37ffc37e8cf812a4e11e71016996d7e22b84414b
Rikki Guy <rikki.guy@gmail.com>
2013-10-31 00:25:04 +0000

    fixed that already

--- src/zmq.d
+++ src/zmq.d
@@ -24,7 +24,6 @@ public class Context {
             s.close();
         
         zmq_ctx_destroy(context);
-        // FIXME : closing a socket causes a segfault?
     }
     
     public Router createRouter() {
